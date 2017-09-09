33498a13fba50fe14536367aa9e050715cc9f8d2
hanksudo <drapho@gmail.com>
2017-09-09 16:46:28 +0900

    filter out unused info

--- osx-wifi-password.sh
+++ osx-wifi-password.sh
@@ -4,9 +4,9 @@ airport=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/
 airportScan=`$airport -I`
 
 SSID=`$airport -I | grep -e "\sSSID:" | awk -F": " '/SSID/{print $2}'`
-
+echo $SSID
 if [[ -n "$SSID" ]]; then
-    security find-generic-password -D "AirPort network password" -g -l "$SSID"
+    security find-generic-password -D "AirPort network password" -g -l "$SSID" 2>&1 >/dev/null
 else
     echo "No Wi-Fi device found!"
 fi
