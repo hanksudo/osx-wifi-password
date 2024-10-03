379547b215579a5acadf9c91c749bcdf5eddad69
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-10-03 12:44:45 +0900

    update shell script

--- osx-wifi-password.sh
+++ osx-wifi-password.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-airport=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
-airportScan=`$airport -I`
-
-SSID=`$airport -I | grep -e "\sSSID:" | awk -F": " '/SSID/{print $2}'`
-echo $SSID
+device=$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $NF}')
+SSID=$(networksetup -getairportnetwork "$device" | cut -d " " -f4)
+echo "$SSID"
 if [[ -n "$SSID" ]]; then
     security find-generic-password -D "AirPort network password" -g -l "$SSID" 2>&1 >/dev/null
 else
